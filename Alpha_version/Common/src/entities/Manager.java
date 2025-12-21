60e0ff167fc488708102e5592778b2cbaaf1864d
Benaya Leib <158835682+BenayaL@users.noreply.github.com>
2025-12-21 19:36:20 +0200

    added employee and manager entitties

---
+++ Alpha_version/Common/src/entities/Manager.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Manager extends Employee {
+	public Manager(String phoneNumber, String email, int memberID, String memberCode, String firstName, String password) {
+		super(phoneNumber, email, memberID, memberCode, firstName, password);
+	}
+	
+}
