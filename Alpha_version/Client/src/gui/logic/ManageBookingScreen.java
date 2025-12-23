71f6b1f4287d3cbdad7f03da73a0dd56f7668292
Uriel Julian Pekelis <urielpekelis@gmail.com>
2025-12-23 21:04:33 +0200

    .

--- Alpha_version/Client/src/gui/logic/ManageBookingScreen.java
+++ Alpha_version/Client/src/gui/logic/ManageBookingScreen.java
@@ -1,6 +1,26 @@
 package gui.logic;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.GridPane;
 
 public class ManageBookingScreen {
+	
+	@FXML
+	private Button btnCheck;
+	@FXML
+	private Button btnBack;
+	@FXML
+	private Button btnCancel;
+	@FXML
+	private DatePicker datePicker;
+	@FXML
+	private ComboBox<String> dinersAmountComboBox;
+	@FXML
+	private GridPane timeSlotsGridPane;
+	
+	private String selectedTimeSlot = null;
 
 }
